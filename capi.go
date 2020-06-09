b1398af36ee618e6994cc1d9c8c1adc9ff219ca4
Yar Kravtsov <yarlson@gmail.com>
2020-06-09 13:27:26 +0300

    Fixes name conventions and linter errors

--- capi.go
+++ capi.go
@@ -21,74 +21,72 @@ import (
 var lock = sync.RWMutex{}
 var listen string
 
-// The bcreate Type. Name of elements must match with jconf params
-type Bcreate struct {
-	Jname                string `json:"jname,omitempty"`
-	Xhci                 string `json:"xhci,omitempty"`
-	Astart               string `json:"astart,omitempty"`
-	Relative_path        string `json:"relative_path,omitempty"`
-	Path                 string `json:"path,omitempty"`
-	Data                 string `json:"data,omitempty"`
-	Rcconf               string `json:"rcconf,omitempty"`
-	Host_hostname        string `json:"host_hostname,omitempty"`
-	Ip4_addr             string `json:"ip4_addr,omitempty"`
-	Nic_hwaddr           string `json:"nic_hwaddr,omitempty"`
-	Zfs_snapsrc          string `json:"zfs_snapsrc,omitempty"`
-	Runasap              string `json:"runasap,omitempty"`
-	Interface            string `json:"interface,omitempty"`
-	Rctl_nice            string `json:"rctl_nice,omitempty"`
-	Emulator             string `json:"emulator,omitempty"`
-	Imgsize              string `json:"imgsize,omitempty"`
-	Imgtype              string `json:"imgtype,omitempty"`
-	Vm_cpus              string `json:"vm_cpus,omitempty"`
-	Vm_ram               string `json:"vm_ram,omitempty"`
-	Vm_os_type           string `json:"vm_os_type,omitempty"`
-	Vm_efi               string `json:"vm_efi,omitempty"`
-	Iso_site             string `json:"iso_site,omitempty"`
-	Iso_img              string `json:"iso_img,omitempty"`
-	Register_iso_name    string `json:"register_iso_name,omitempty"`
-	Register_iso_as      string `json:"register_iso_as,omitempty"`
-	Vm_hostbridge        string `json:"vm_hostbridge,omitempty"`
-	Bhyve_flags          string `json:"bhyve_flags,omitempty"`
-	Virtio_type          string `json:"virtio_type,omitempty"`
-	Vm_os_profile        string `json:"vm_os_profile,omitempty"`
-	Swapsize             string `json:"swapsize,omitempty"`
-	Vm_iso_path          string `json:"vm_iso_path,omitempty"`
-	Vm_guestfs           string `json:"vm_guestfs,omitempty"`
-	Vm_vnc_port          string `json:"vm_vnc_port,omitempty"`
-	Bhyve_generate_acpi  string `json:"bhyve_generate_acpi,omitempty"`
-	Bhyve_wire_memory    string `json:"bhyve_wire_memory,omitempty"`
-	Bhyve_rts_keeps_utc  string `json:"bhyve_rts_keeps_utc,omitempty"`
-	Bhyve_force_msi_irq  string `json:"bhyve_force_msi_irq,omitempty"`
-	Bhyve_x2apic_mode    string `json:"bhyve_x2apic_mode,omitempty"`
-	Bhyve_mptable_gen    string `json:"bhyve_mptable_gen,omitempty"`
-	Bhyve_ignore_msr_acc string `json:"bhyve_ignore_msr_acc,omitempty"`
-	Cd_vnc_wait          string `json:"cd_vnc_wait,omitempty"`
-	Bhyve_vnc_resolution string `json:"bhyve_vnc_resolution,omitempty"`
-	Bhyve_vnc_tcp_bind   string `json:"bhyve_vnc_tcp_bind,omitempty"`
-	Bhyve_vnc_vgaconf    string `json:"bhyve_vnc_vgaconf,omitempty"`
-	Nic_driver           string `json:"nic_driver,omitempty"`
-	Vnc_password         string `json:"vnc_password,omitempty"`
-	Media_auto_eject     string `json:"media_auto_eject,omitempty"`
-	Vm_cpu_topology      string `json:"vm_cpu_topology,omitempty"`
-	Debug_engine         string `json:"debug_engine,omitempty"`
-	Cd_boot_firmware     string `json:"cd_boot_firmware,omitempty"`
-	Jailed               string `json:"jailed,omitempty"`
-	On_poweroff          string `json:"on_poweroff,omitempty"`
-	On_reboot            string `json:"on_reboot,omitempty"`
-	On_crash             string `json:"on_crash,omitempty"`
+// The BCreate Type. Name of elements must match with jconf params
+type BCreate struct {
+	JName              string `json:"jname,omitempty"`
+	XHCI               string `json:"xhci,omitempty"`
+	AStart             string `json:"astart,omitempty"`
+	RelativePath       string `json:"relative_path,omitempty"`
+	Path               string `json:"path,omitempty"`
+	Data               string `json:"data,omitempty"`
+	RCConf             string `json:"rcconf,omitempty"`
+	HostHostname       string `json:"host_hostname,omitempty"`
+	IP4Addr            string `json:"ip4_addr,omitempty"`
+	NicHWAddr          string `json:"nic_hwaddr,omitempty"`
+	ZfsSnapSrc         string `json:"zfs_snapsrc,omitempty"`
+	RunASAP            string `json:"runasap,omitempty"`
+	Interface          string `json:"interface,omitempty"`
+	RCtlNice           string `json:"rctl_nice,omitempty"`
+	Emulator           string `json:"emulator,omitempty"`
+	ImgSize            string `json:"imgsize,omitempty"`
+	ImgType            string `json:"imgtype,omitempty"`
+	VmCPUs             string `json:"vm_cpus,omitempty"`
+	VmRAM              string `json:"vm_ram,omitempty"`
+	VmOSType           string `json:"vm_os_type,omitempty"`
+	VmEFI              string `json:"vm_efi,omitempty"`
+	IsoSite            string `json:"iso_site,omitempty"`
+	IsoImg             string `json:"iso_img,omitempty"`
+	RegisterIsoName    string `json:"register_iso_name,omitempty"`
+	RegisterIsoAs      string `json:"register_iso_as,omitempty"`
+	VmHostBridge       string `json:"vm_hostbridge,omitempty"`
+	BhyveFlags         string `json:"bhyve_flags,omitempty"`
+	VirtioType         string `json:"virtio_type,omitempty"`
+	VmOSProfile        string `json:"vm_os_profile,omitempty"`
+	SwapSize           string `json:"swapsize,omitempty"`
+	VmIsoPath          string `json:"vm_iso_path,omitempty"`
+	VmGuestFS          string `json:"vm_guestfs,omitempty"`
+	VmVNCPort          string `json:"vm_vnc_port,omitempty"`
+	BhyveGenerateAcpi  string `json:"bhyve_generate_acpi,omitempty"`
+	BhyveWireMemory    string `json:"bhyve_wire_memory,omitempty"`
+	BhyveRtsKeepsUtc   string `json:"bhyve_rts_keeps_utc,omitempty"`
+	BhyveForceMsiIrq   string `json:"bhyve_force_msi_irq,omitempty"`
+	BhyveX2ApicMode    string `json:"bhyve_x2apic_mode,omitempty"`
+	BhyveMpTableGen    string `json:"bhyve_mptable_gen,omitempty"`
+	BhyveIgnoreMsrAcc  string `json:"bhyve_ignore_msr_acc,omitempty"`
+	CdVncWait          string `json:"cd_vnc_wait,omitempty"`
+	BhyveVNCResolution string `json:"bhyve_vnc_resolution,omitempty"`
+	BhyveVNCTcpBind    string `json:"bhyve_vnc_tcp_bind,omitempty"`
+	BhyveVNCVgaConf    string `json:"bhyve_vnc_vgaconf,omitempty"`
+	NicDriver          string `json:"nic_driver,omitempty"`
+	VNCPassword        string `json:"vnc_password,omitempty"`
+	MediaAutoEject     string `json:"media_auto_eject,omitempty"`
+	VmCPUTopology      string `json:"vm_cpu_topology,omitempty"`
+	DebugEngine        string `json:"debug_engine,omitempty"`
+	CdBootFirmware     string `json:"cd_boot_firmware,omitempty"`
+	Jailed             string `json:"jailed,omitempty"`
+	OnPowerOff         string `json:"on_poweroff,omitempty"`
+	OnReboot           string `json:"on_reboot,omitempty"`
+	OnCrash            string `json:"on_crash,omitempty"`
 }
 
-var people []Bcreate
-
 type Bhyves struct {
-	Jname      string
-	Jid        int
-	Vm_Ram     int // MB
-	Vm_Cpus    int
-	Vm_Os_Type string
-	Status     string
-	Vnc        string
+	JName    string
+	JID      int
+	VmRam    int // MB
+	VmCPUs   int
+	VmOSType string
+	Status   string
+	VNC      string
 }
 
 var bhyves []Bhyves
@@ -148,23 +146,20 @@ func HandleBhyveList(w http.ResponseWriter, r *http.Request) {
 	for _, line := range lines {
 		if len(line) > 2 {
 			ima := Bhyves{}
-			re_leadclose_whtsp := regexp.MustCompile(`^[\s\p{Zs}]+|[\s\p{Zs}]+$`)
-			re_inside_whtsp := regexp.MustCompile(`[\s\p{Zs}]{2,}`)
-			n := re_leadclose_whtsp.ReplaceAllString(line, "")
-			n = re_inside_whtsp.ReplaceAllString(line, " ")
-			ima.Jname = strings.Split(n, " ")[0]
-			ima.Jid, err = strconv.Atoi(strings.Split(n, " ")[1])
-			ima.Vm_Ram, err = strconv.Atoi(strings.Split(n, " ")[2])
-			ima.Vm_Cpus, err = strconv.Atoi(strings.Split(n, " ")[3])
-			ima.Vm_Os_Type = strings.Split(n, " ")[4]
+			reInsideWhtsp := regexp.MustCompile(`[\s\p{Zs}]{2,}`)
+			n := reInsideWhtsp.ReplaceAllString(line, " ")
+			ima.JName = strings.Split(n, " ")[0]
+			ima.JID, _ = strconv.Atoi(strings.Split(n, " ")[1])
+			ima.VmRam, _ = strconv.Atoi(strings.Split(n, " ")[2])
+			ima.VmCPUs, _ = strconv.Atoi(strings.Split(n, " ")[3])
+			ima.VmOSType = strings.Split(n, " ")[4]
 			ima.Status = strings.Split(n, " ")[5]
-			ima.Vnc = strings.Split(n, " ")[6]
+			ima.VNC = strings.Split(n, " ")[6]
 			imas = append(imas, ima)
 		}
 	}
 
-	//w.WriteJson(imas)
-	json.NewEncoder(w).Encode(&imas)
+	_ = json.NewEncoder(w).Encode(&imas)
 }
 
 func HandleInitBhyveList() {
@@ -182,41 +177,37 @@ func HandleInitBhyveList() {
 	for _, line := range lines {
 		if len(line) > 2 {
 			ima := Bhyves{}
-			re_leadclose_whtsp := regexp.MustCompile(`^[\s\p{Zs}]+|[\s\p{Zs}]+$`)
-			re_inside_whtsp := regexp.MustCompile(`[\s\p{Zs}]{2,}`)
-			n := re_leadclose_whtsp.ReplaceAllString(line, "")
-			n = re_inside_whtsp.ReplaceAllString(line, " ")
-			ima.Jname = strings.Split(n, " ")[0]
-			ima.Jid, err = strconv.Atoi(strings.Split(n, " ")[1])
-			ima.Vm_Ram, err = strconv.Atoi(strings.Split(n, " ")[2])
-			ima.Vm_Cpus, err = strconv.Atoi(strings.Split(n, " ")[3])
-			ima.Vm_Os_Type = strings.Split(n, " ")[4]
+			reInsideWhtsp := regexp.MustCompile(`[\s\p{Zs}]{2,}`)
+			n := reInsideWhtsp.ReplaceAllString(line, " ")
+			ima.JName = strings.Split(n, " ")[0]
+			ima.JID, _ = strconv.Atoi(strings.Split(n, " ")[1])
+			ima.VmRam, _ = strconv.Atoi(strings.Split(n, " ")[2])
+			ima.VmCPUs, _ = strconv.Atoi(strings.Split(n, " ")[3])
+			ima.VmOSType = strings.Split(n, " ")[4]
 			ima.Status = strings.Split(n, " ")[5]
-			ima.Vnc = strings.Split(n, " ")[6]
+			ima.VNC = strings.Split(n, " ")[6]
 			bhyves = append(bhyves, ima)
 		}
 	}
 }
 
 func HandleCacheBhyveList(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(&bhyves)
+	_ = json.NewEncoder(w).Encode(&bhyves)
 }
 
 func HandleBhyveStart(w http.ResponseWriter, r *http.Request) {
 
 	params := mux.Vars(r)
-	var instanceid string
-	_ = json.NewDecoder(r.Body).Decode(&instanceid)
-	instanceid = params["instanceid"]
+	var instanceID string
+	_ = json.NewDecoder(r.Body).Decode(&instanceID)
+	instanceID = params["instanceid"]
 
-	go realInstanceStart(instanceid)
-
-	return
+	go realInstanceStart(instanceID)
 }
 
-func realInstanceStart(instanceid string) {
-	jname := "jname=" + instanceid
-	cmd := exec.Command("env", "NOCOLOR=0", "cbsd", "bstart", "inter=0", jname)
+func realInstanceStart(instanceID string) {
+	jName := "jname=" + instanceID
+	cmd := exec.Command("env", "NOCOLOR=0", "cbsd", "bstart", "inter=0", jName)
 	_, err := cmd.Output()
 
 	if err != nil {
@@ -227,7 +218,7 @@ func realInstanceStart(instanceid string) {
 func HandleBhyveStop(w http.ResponseWriter, r *http.Request) {
 
 	params := mux.Vars(r)
-	var instanceid string
+	var instanceID string
 	buf, bodyErr := ioutil.ReadAll(r.Body)
 
 	if bodyErr != nil {
@@ -241,17 +232,15 @@ func HandleBhyveStop(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("BODY rdr1: %q", rdr1)
 	r.Body = rdr2
 
-	_ = json.NewDecoder(r.Body).Decode(&instanceid)
-	instanceid = params["instanceid"]
+	_ = json.NewDecoder(r.Body).Decode(&instanceID)
+	instanceID = params["instanceid"]
 
-	go realInstanceStop(instanceid)
-
-	return
+	go realInstanceStop(instanceID)
 }
 
 func realInstanceStop(instanceid string) {
-	jname := "jname=" + instanceid
-	cmd := exec.Command("env", "NOCOLOR=0", "cbsd", "bstop", "inter=0", jname)
+	jName := "jname=" + instanceid
+	cmd := exec.Command("env", "NOCOLOR=0", "cbsd", "bstop", "inter=0", jName)
 	_, err := cmd.Output()
 
 	if err != nil {
@@ -262,7 +251,7 @@ func realInstanceStop(instanceid string) {
 func HandleBhyveRemove(w http.ResponseWriter, r *http.Request) {
 
 	params := mux.Vars(r)
-	var instanceid string
+	var instanceID string
 	buf, bodyErr := ioutil.ReadAll(r.Body)
 
 	if bodyErr != nil {
@@ -278,16 +267,14 @@ func HandleBhyveRemove(w http.ResponseWriter, r *http.Request) {
 	//fmt.Printf("BODY rdr2: %q", rdr2)
 	r.Body = rdr2
 
-	_ = json.NewDecoder(r.Body).Decode(&instanceid)
-	instanceid = params["instanceid"]
-	go realInstanceRemove(instanceid)
-
-	return
+	_ = json.NewDecoder(r.Body).Decode(&instanceID)
+	instanceID = params["instanceid"]
+	go realInstanceRemove(instanceID)
 }
 
 func realInstanceRemove(instanceid string) {
-	jname := "jname=" + instanceid
-	cmd := exec.Command("env", "NOCOLOR=0", "cbsd", "bremove", "inter=0", jname)
+	jName := "jname=" + instanceid
+	cmd := exec.Command("env", "NOCOLOR=0", "cbsd", "bremove", "inter=0", jName)
 	_, err := cmd.Output()
 
 	if err != nil {
@@ -314,30 +301,26 @@ func realInstanceCreate(createstr string) {
 	}
 }
 
-func getStructTag(f reflect.StructField) string {
-	return string(f.Tag)
-}
-
 func HandleBhyveCreate(w http.ResponseWriter, r *http.Request) {
-	var instanceid string
+	var instanceID string
 	params := mux.Vars(r)
-	instanceid = params["instanceid"]
+	instanceID = params["instanceid"]
 
 	if r.Body == nil {
 		http.Error(w, "Please send a request body", 400)
 		return
 	}
 
-	var bcreate Bcreate
-	_ = json.NewDecoder(r.Body).Decode(&bcreate)
-	bcreate.Jname = instanceid
-	json.NewEncoder(w).Encode(bcreate)
-	val := reflect.ValueOf(bcreate)
+	var bCreate BCreate
+	_ = json.NewDecoder(r.Body).Decode(&bCreate)
+	bCreate.JName = instanceID
+	_ = json.NewEncoder(w).Encode(bCreate)
+	val := reflect.ValueOf(bCreate)
 
-	//	fmt.Println("J ",bcreate.Jname)
-	//	fmt.Println("R ",bcreate.Vm_ram)
+	//	fmt.Println("J ",bCreate.JName)
+	//	fmt.Println("R ",bCreate.VmRAM)
 
-	var jconf_param string
+	var jConfParam string
 	var str strings.Builder
 
 	str.WriteString("env NOCOLOR=1 /usr/local/bin/cbsd bcreate inter=0 ")
@@ -355,13 +338,11 @@ func HandleBhyveCreate(w http.ResponseWriter, r *http.Request) {
 			continue
 		}
 
-		jconf_param = strings.ToLower(typeField.Name)
-		fmt.Printf("jconf: %s,\tField Name: %s,\t Field Value: %v,\t Tag Value: %s\n", jconf_param, typeField.Name, valueField.Interface(), tag.Get("tag_name"))
-		buf := fmt.Sprintf("%s=%v ", jconf_param, tmpval)
+		jConfParam = strings.ToLower(typeField.Name)
+		fmt.Printf("jconf: %s,\tField Name: %s,\t Field Value: %v,\t Tag Value: %s\n", jConfParam, typeField.Name, valueField.Interface(), tag.Get("tag_name"))
+		buf := fmt.Sprintf("%s=%v ", jConfParam, tmpval)
 		str.WriteString(buf)
 	}
 
 	go realInstanceCreate(str.String())
-
-	return
 }
